11b593ff84828880d62c35f6893ea91f3073730a
Boyacto <bohyeoncha2000@gmail.com>
2021-06-07 21:59:52 -0500

    Update: created model for mongoose and changed views, etc

--- src/server.js
+++ src/server.js
@@ -1,11 +1,13 @@
 import express from "express"; // babel will make to work
 import morgan from "morgan";
+
+
+
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
-import "./db";
 
-const PORT = 4000;
+
 
 const app = express(); // creates express application
 const logger = morgan("dev")
@@ -18,9 +20,4 @@ app.use("/", globalRouter);
 app.use("/videos",videoRouter);
 app.use("/users", userRouter);
 
-
-const handleListening = () => console.log(`Server listening on port http//localhost:${PORT}`);
-
-app.listen(PORT, handleListening);
-//app.listen(4000, () => console.log("Server listening on port 4000") ); does the same thing
-
+export default app;
